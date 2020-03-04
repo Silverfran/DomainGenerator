60a3cda78689bd2c0abc916792160ed3874c9847
SilverFran <Francisco1627@gmail.com>
2020-03-04 01:48:06 +0000

    final result

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,17 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  let pronoun = ["I", "We", "They", "Me", "You"];
+  let adjective = ["small", "large", "round", "wealthy", "slow"];
+  let noun = ["student", "teacher", "minister", "businessperson", "salesclerk"];
+  let arrayResult = "";
+  for (let i of pronoun) {
+    for (let j of adjective) {
+      for (let z of noun) {
+        arrayResult += "www." + i + j + z + ".com<br>";
+      }
+    }
+  }
+
+  document.getElementById("result").innerHTML = arrayResult;
 };
